1a09d415dbc07b62b2c4ad394336baaa97dc5d02
Serg <Serg.srav@mail.ru>
2023-05-22 16:38:02 +0500

    добавили генератор случайных чисел

--- prog4/Program.cs
+++ prog4/Program.cs
@@ -1,3 +1,5 @@
-﻿double numberA = 12;
-double numberB = 5;
+﻿double numberA = new Random().Next(1,10);// случайное число 0,1,2,....9
+double numberB = new Random().Next(1,10);
+Console.WriteLine(numberA);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA/numberB);
